2fd06558f13523d8e380aaac3f94503837f0971a
koplu <onlinetraining99831@gmail.com>
2019-01-26 15:50:13 +0530

    INitial commit

---
+++ ClassLibrary1/Framework/Page.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary1.POM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Framework
+{
+    public class Page
+    {
+        public static LoginPage Loginpage = new LoginPage(Driver.getdriver());
+        public static HomePage Homepage = new HomePage(Driver.getdriver());
+        public static OrdersPage Orderspage = new OrdersPage(Driver.getdriver());
+    }
+}
